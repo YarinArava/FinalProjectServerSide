5ed7e96a17d3cf57c66835cd0ace06766c214b87
yarinarava <yarinarava@gmail.com>
2025-10-03 22:50:34 +0300

    added googleMaps link

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86501448d656290d557dc32c0de77cbff43bc234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b475fdf98f39a6852225fb9a7f1cdb4ed44ef912")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
